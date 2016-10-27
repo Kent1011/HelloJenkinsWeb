e2cd50b87fab5395108813728118c6c6c9072c20
Kent Chien <kent1011@gmail.com>
2016-10-27 20:13:01 +0800

    change text

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('Hello Jenkins');
+  res.send('Hello Jenkins!');
 });
 
 module.exports = router;
